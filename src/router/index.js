3acd4deacaa41cc2217ad40b74dbca5bbcda941b
CRR <ruichen_only@163.com>
2022-01-06 23:47:23 +0800

    修改文件格式

--- src/router/index.js
+++ src/router/index.js
@@ -50,7 +50,7 @@ export const constantRoutes = [
     children: [
       {
         path: 'dashboard',
-        name: 'Dashboard',
+        name: '主页',
         component: () => import('@/views/dashboard/index'),
         meta: { title: 'Dashboard', icon: 'dashboard' },
       },
